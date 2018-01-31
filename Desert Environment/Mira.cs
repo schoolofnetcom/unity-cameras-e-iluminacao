05c3491a364a5ec974367e8a4c18783225f5ed92
techtuxbr <victordevtb@gmail.com>
2018-01-31 04:13:54 -0200

    first commit

---
+++ Desert Environment/Mira.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mira : MonoBehaviour {
+
+    public Camera cam;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            cam.fieldOfView = 12f;
+        }
+
+	}
+}
